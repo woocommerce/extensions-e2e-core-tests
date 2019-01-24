1e356b608d36b58f678ba1ab49199eefee3c785c
Fernando Espinosa <fer@ferdev.com>
2019-01-24 18:23:33 +0000

    Updates run-e2e-CI.sh
    Now the script can take parameters to take care of the building phases
    needed to run the WC e2e tests

--- bin/run-e2e-CI.sh
+++ bin/run-e2e-CI.sh
@@ -6,5 +6,26 @@ fi
 
 E2E_FOLDER=${E2E_FOLDER:-tests}
 
-cd $E2E_FOLDER/woocommerce
-. tests/bin/run-e2e-CI.sh
+ADMIN_USERNAME=${ADMIN_USERNAME:-admin}
+ADMIN_PASSWORD=${ADMIN_PASSWORD:-password}
+CUSTOMER_USERNAME=${CUSTOMER_USERNAME:-customer}
+CUSTOMER_PASSWORD=${CUSTOMER_PASSWORD:-password}
+
+
+#!/bin/bash
+
+case $1 in
+  install)
+    git clone --branch=master git@github.com:woocommerce/extensions-e2e-core-tests.git $E2E_FOLDER
+  ;;
+  before)
+    export PATH="$HOME/.composer/vendor/bin:$PATH"
+    export NODE_CONFIG="{\"users\":{\"admin\":{\"username\":$ADMIN_USERNAME,\"password\":$ADMIN_PASSWORD},\"customer\":{\"username\":$CUSTOMER_USERNAME,\"password\":$CUSTOMER_PASSWORD}}}"
+    $E2E_FOLDER/bin/install.sh woocommerce_test root '' localhost $WP_VERSION
+    $E2E_FOLDER/bin/travis.sh before
+  ;;
+  run)
+    cd $E2E_FOLDER/woocommerce
+    . tests/bin/run-e2e-CI.sh
+  ;;
+esac
